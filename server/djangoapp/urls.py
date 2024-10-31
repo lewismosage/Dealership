b3910247369f1af8e9bc6b9ab09ec405a4a23bab
Lewismosage <lewismosage@users.noreply.github.com>
2024-10-31 11:58:03 +0300

    Updated Car Models

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,21 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
+     path(route='login', view=views.login_user, name='login'),
+     # Path for logout
+     path(route='logout', view=views.logout_request, name='logout'),
     # path for dealer reviews view
-
+     path(route='register', view=views.registration, name='register'),
+    # path for get_cars
+     path('get_cars/', views.get_cars, name='get_cars'),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
